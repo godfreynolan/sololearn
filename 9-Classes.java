935538716d94bc9608a44eb6c9297512da201e70
Godfrey Nolan <godfrey@riis.com>
2023-11-30 11:26:02 -0500

    Create 9-Classes.java

---
+++ 9-Classes.java
@@ -0,0 +1,24 @@
+
+public class Main {
+
+   public static void main(String[] args) {
+
+    /* Create a program to show loading message to your application users.
+    Define a class Loading which has one public method called LoadingMessage(), which should print "Loading" when called.
+    Create an object named loading and call that method. */
+
+       //create a Loading object with the same name
+
+       loading.LoadingMessage();
+
+   }
+
+}
+
+
+
+class Loading {
+
+     //complete the class, add LoadingMessage() method
+
+}
