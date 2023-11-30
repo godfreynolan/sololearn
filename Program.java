b845b96079c112b26f2ae56e09eb7ddb8e2d302e
Godfrey Nolan <godfrey@riis.com>
2023-11-30 10:06:33 -0500

    Update Program.java

--- Program.java
+++ Program.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 public class Program {
 	public static void main(String[] args) {
 
-		      // Scanner scanner = new Scanner(System.in);
-		      // int days = scanner.nextInt();
-		      //your code goes here
+		// Scanner scanner = new Scanner(System.in);
+		// int days = scanner.nextInt();
+		//your code goes here
         	System.out.println("Hello World");
-		      // 24 * 60 * 60
+		// 24 * 60 * 60
 	}
 }
