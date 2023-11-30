dabcc1cdd501fb1f0eaceed2cf85dd58934cb027
Godfrey Nolan <godfrey@riis.com>
2023-11-30 11:19:48 -0500

    Create 5-WhileLoops.java

---
+++ 5-WhileLoops.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+            int  number = scanner.nextInt();
+            int fact = 1;
+            //your code goes here
+           
+    }
+}
