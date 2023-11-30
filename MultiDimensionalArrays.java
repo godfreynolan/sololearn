b967402e53eca84058de0fdaa1710834db65da17
Godfrey Nolan <godfrey@riis.com>
2023-11-30 11:21:45 -0500

    Create MultiDimensionalArrays.java

---
+++ MultiDimensionalArrays.java
@@ -0,0 +1,12 @@
+
+public class Main {
+   public static void main(String[] args) {
+       int[][] matrix = {
+               {8, 1, 6},
+               {3, 5, 7},
+               {4, 9, 0},
+       };
+       //output the numbers
+       
+   }
+}
