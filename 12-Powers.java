0f021f36dface914abc2d2c9f5a367d53f173ae1
Godfrey Nolan <godfrey@riis.com>
2023-11-30 11:28:33 -0500

    Create 12-Powers.java

---
+++ 12-Powers.java
@@ -0,0 +1,13 @@
+
+import java.util.Scanner;
+
+class Main {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       int num1 = read.nextInt();
+       int num2 = read.nextInt();
+
+       //your code goes here
+       //double p = Math.pow(2, 3);
+   }
+}
