f11a65fbe803af6367ac654d0fd322ad2e10bada
Godfrey Nolan <godfrey@riis.com>
2023-11-30 10:40:02 -0500

    Create NestedIfElseStatements.java

---
+++ NestedIfElseStatements.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+       Scanner read = new Scanner(System.in);
+       int purchases = read.nextInt();
+       
+       if(purchases > 15000){
+        System.out.println("Gift card");
+        //complete the code
+       
+        }
+    }
+}
