c0ed5aa1e5da05186af6c54256711fcda082de61
Godfrey Nolan <godfrey@riis.com>
2023-11-30 11:21:10 -0500

    Create 6-ForEachLoop.java

---
+++ 6-ForEachLoop.java
@@ -0,0 +1,16 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int length = scanner.nextInt();
+       int[] sides =  new int[length];
+       for (int i = 0; i < length; i++) {
+           sides[i] = scanner.nextInt();
+       }
+      //your code goes here
+     
+   }
+}
