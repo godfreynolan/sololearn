af8d4804166e6dc4b611ae4cdc93dae554d37bc5
Godfrey Nolan <godfrey@riis.com>
2023-11-30 10:44:23 -0500

    Create SwitchStatement.java

---
+++ SwitchStatement.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+   public static void main(String[] args) {
+       Scanner scanner = new Scanner(System.in);
+       int emotion = scanner.nextInt();
+       /*
+       1 - "You are happy!"
+       2 - "You are sad!"
+       3 - "You are angry!"
+       4 - "You are surprised!"
+       other - "Unknown emotion."
+       */
+       
+       // your code goes here
+       
+       
+   }
+}
